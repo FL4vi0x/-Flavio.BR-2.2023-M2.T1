ad726223c01e8b46d03af120ae39a8c0efb207e6
Flávio José <flavioolindapt@hotmail.com>
2023-07-20 00:48:37 -0300

    comit da aula #05

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -7,9 +7,11 @@ from dino_runner.utils.constants import (
     SCREEN_WIDTH,
     TITLE,
     FPS,
+    DEFAULT_TYPE,
 )
 from dino_runner.components.dinosaur import Dinosaur
 from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
+from dino_runner.components.power_ups.power_up_manager import PowerUpManager
 
 FONT_STYLE = "freesansbold.ttf"
 
@@ -30,6 +32,7 @@ class Game:
         self.y_pos_bg = 380
         self.player = Dinosaur()
         self.obstacle_manager = ObstacleManager()
+        self.power_up_manager = PowerUpManager()
 
     def template_text(self, text_data, color, position):
         font = pygame.font.Font(FONT_STYLE, 22)
@@ -52,6 +55,7 @@ class Game:
         self.score = 0
         self.game_speed = 20
         self.obstacle_manager.reset_obstacles()
+        self.power_up_manager.reset_power_ups()
         while self.playing:
             self.events()
             self.update()
@@ -67,6 +71,7 @@ class Game:
         user_input = pygame.key.get_pressed()
         self.player.update(user_input)
         self.obstacle_manager.update(self)
+        self.power_up_manager.update(self)
         self.update_score()
 
     def update_score(self):
@@ -80,7 +85,9 @@ class Game:
         self.draw_background()
         self.player.draw(self.screen)
         self.obstacle_manager.draw(self.screen)
+        self.power_up_manager.draw(self.screen)
         self.draw_score()
+        self.draw_power_up_time()
         pygame.display.update()
         pygame.display.flip()
 
@@ -96,6 +103,21 @@ class Game:
     def draw_score(self):
         self.template_text(f"Score: {self.score}", (0, 0, 0), (1000, 50))
 
+    def draw_power_up_time(self):
+        if self.player.has_power_up:
+            time_to_show = round(
+                (self.player.power_up_time - pygame.time.get_ticks()) / 1000, 1
+            )
+            if time_to_show >= 0:
+                self.template_text(
+                    f"{self.player.type.capitalize()} enabled for {time_to_show} seconds",
+                    (0, 0, 0),
+                    (500, 40),
+                )
+            else:
+                self.player.has_power_up = False
+                self.player.type = DEFAULT_TYPE
+
     def show_menu(self):
         self.screen.fill((255, 255, 255))
         half_screen_height = SCREEN_HEIGHT // 2
@@ -109,54 +131,54 @@ class Game:
             )
 
         else:  ## tela de restart      #    x                              y
-            self.screen.blit(ICON, (half_screen_width - 40, half_screen_height - 140))
+            self.screen.blit(ICON, (half_screen_width - 40, half_screen_height - 110))
             if self.death_count == 1:
                 self.template_text(
-                    'you were from "F"',
+                    'You were from "F"',
+                    (0, 0, 0),
+                    (half_screen_width, half_screen_height + 60),
+                )
+                self.template_text(
+                    "Press any key to restart",
                     (0, 0, 0),
                     (half_screen_width, half_screen_height),
                 )
                 self.template_text(
-                    "Press any key to restart",
-                    (0, 0, 0),
-                    (half_screen_width, half_screen_height + 60),
-                )
-                self.template_text(
                     f"Score achieved: {self.score}",
                     (0, 0, 0),
-                    (half_screen_width, half_screen_height + 30),
+                    (half_screen_width, half_screen_height - 150),
                 )
             elif self.death_count == 2:
                 self.template_text(
-                    'you were from "F" twice',
+                    'You were from "F" twice',
                     (0, 0, 0),
                     (half_screen_width, half_screen_height + 60),
                 )
                 self.template_text(
                     f"Score achieved: {self.score}",
-                    (0, 0, 0),
-                    (half_screen_width, half_screen_height + 90),
+                    (225, 0, 60),
+                    (half_screen_width, half_screen_height - 150),
                 )
                 self.template_text(
                     "Press any key to restart",
                     (0, 0, 0),
-                    (half_screen_width, half_screen_height + 120),
+                    (half_screen_width, half_screen_height),
                 )
             else:
                 self.template_text(
-                    f'you were from "F" {self.death_count} times',
+                    f'You were from "F" {self.death_count} times',
                     (0, 0, 0),
                     (half_screen_width, half_screen_height + 60),
                 )
                 self.template_text(
                     f"Score achieved: {self.score}",
                     (0, 0, 0),
-                    (half_screen_width, half_screen_height + 90),
+                    (half_screen_width, half_screen_height - 150),
                 )
                 self.template_text(
                     "Press any key to restart",
                     (0, 0, 0),
-                    (half_screen_width, half_screen_height + 120),
+                    (half_screen_width, half_screen_height),
                 )
             ## mostrar mensagem de "Press any key to restart"
             ## mostrar o score atingido
